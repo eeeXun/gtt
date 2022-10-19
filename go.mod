a29f1c2f867ed0e326dc7ac9195f7c7c436aab18
eeeXun <sdes96303@gmail.com>
2022-10-19 14:45:35 +0800

    sound support

--- go.mod
+++ go.mod
@@ -3,13 +3,18 @@ module gtt
 go 1.19
 
 require (
+	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1
+	github.com/hajimehoshi/go-mp3 v0.3.3
+	github.com/hajimehoshi/oto/v2 v2.3.1
+	github.com/rivo/tview v0.0.0-20220911190240-55965cf21d8e
+)
+
+require (
 	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/rivo/tview v0.0.0-20220911190240-55965cf21d8e // indirect
 	github.com/rivo/uniseg v0.4.2 // indirect
-	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
+	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
 	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- translator.go
+++ translator.go
@@ -4,13 +4,16 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto/v2"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 const (
-	api_url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=%s&tl=%s&dt=t&q=%s"
+	api_url   = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=%s&tl=%s&dt=t&q=%s"
 	sound_url = "https://translate.google.com.vn/translate_tts?ie=UTF-8&q=%s&tl=%s&client=tw-ob"
 )
 
@@ -52,4 +55,34 @@ func (t Translator) Translate(message string) (string, error) {
 		return translated, nil
 	}
 	return "", errors.New("Translation not found")
+}
+
+func (t Translator) PlaySound(lang string, message string) {
+	url_str := fmt.Sprintf(
+		sound_url,
+		url.QueryEscape(message),
+		Lang_Code[lang],
+	)
+	res, err := http.Get(url_str)
+	if err != nil {
+		panic(err)
+	}
+	decoder, err := mp3.NewDecoder(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	otoCtx, readyChan, err := oto.NewContext(decoder.SampleRate(), 2, 2)
+	if err != nil {
+		panic(err)
+	}
+	<-readyChan
+	player := otoCtx.NewPlayer(decoder)
+	player.Play()
+	for player.IsPlaying() {
+		time.Sleep(time.Second)
+	}
+	err = player.Close()
+	if err != nil {
+		panic(err)
+	}
 }
