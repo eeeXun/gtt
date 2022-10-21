681f349fc4331cff618cc6855d2db59d5312b906
eeeXun <sdes96303@gmail.com>
2022-10-22 01:45:21 +0800

    fix swap text error

--- ui.go
+++ ui.go
@@ -125,7 +125,7 @@ func TranslatePageHandler(event *tcell.EventKey) *tcell.EventKey {
 		updateTitle()
 		src_text := src_box.GetText()
 		dst_text := dst_box.GetText(false)
-		src_box.SetText(dst_text, true)
+		src_box.SetText(dst_text[:len(dst_text)-1], true)
 		dst_box.SetText(src_text)
 	}
 
