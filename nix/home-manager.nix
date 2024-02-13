13e16aa24a847ca7d7c53e33641b4c8d897d4ca9
TornaxO7 <tornax@proton.me>
2024-02-13 18:11:46 +0100

    nix: extend config options for home manager

--- nix/home-manager.nix
+++ nix/home-manager.nix
@@ -25,14 +25,62 @@ in
         {file}`$XDG_CONFIG_HOME/.config/gtt/server.yaml`.
       '';
     };
+
+    theme = mkOption {
+      type = yamlFormat.type;
+      default = { };
+      example = literalExpression ''
+        {
+          tokyonight = {
+            bg = "0x1a1b26";
+            fg = "0xc0caf5";
+            gray = "0x414868";
+            red = "0xf7768e";
+            green = "0x9ece6a";
+            yellow = "0xe0af68";
+            blue = "0x7aa2f7";
+            purple = "0xbb9af7";
+            cyan = "0x7dcfff";
+            orange = "0xff9e64";         
+          };
+        }
+      '';
+
+      description = ''
+        Theme written to
+        {file}`$XDG_CONFIG_HOME/.config/gtt/theme.yaml`.
+      '';
+    };
+
+
+    keymap = mkOption {
+      type = yamlFormat.type;
+      default = { };
+      example = literalExpression ''
+        {
+          exit = "C-c";
+          translate = "C-j";
+        }
+      '';
+    };
   };
 
   config = mkIf cfg.enable
     {
       home.packages = with pkgs; [ gtt ];
 
-      xdg.configFile."gtt/server.yaml" = mkIf (cfg.settings != { }) {
-        source = yamlFormat.generate "server.yaml" cfg.settings;
+      xdg.configFile = {
+        "gtt/server.yaml" = mkIf (cfg.settings != { }) {
+          source = yamlFormat.generate "server.yaml" cfg.settings;
+        };
+
+        "gtt/theme.yaml" = mkIf (cfg.theme != { }) {
+          source = yamlFormat.generate "theme.yaml" cfg.theme;
+        };
+
+        "gtt/keymap.yaml" = mkIf (cfg.keymap != { }) {
+          source = yamlFormat.generate "keymap.yaml" cfg.theme;
+        };
       };
     };
 }
